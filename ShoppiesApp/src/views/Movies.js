498ad88d6e5cf7575b8403af4a17da497e6a5150
tyrellshawn <ooluk098@uottawa.ca>
2020-12-15 20:45:13 -0500

    first commit

---
+++ ShoppiesApp/src/views/Movies.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+// reactstrap components
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardTitle,
+  Table,
+  Row,
+  Col,
+} from "reactstrap";
+
+function Movies() {
+  return (
+    <>
+      <div className="content">
+        <Row>
+          <Col md="12">
+            <Card>
+              <CardHeader>
+                <CardTitle tag="h4">Popular Movies</CardTitle>
+              </CardHeader>
+              <CardBody>
+              </CardBody>
+            </Card>
+          </Col>
+          <Col md="12">
+            <Card className="card-plain">
+              <CardHeader>
+                <CardTitle tag="h4">Action Movies</CardTitle>
+                <p className="category">Explosive Flicks</p>
+              </CardHeader>
+              <CardBody>
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+      </div>
+    </>
+  );
+}
+
+export default Movies;
---
+++ ShoppiesApp/src/views/Search.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+// reactstrap components
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardTitle,
+  Row,
+  Col,
+} from "reactstrap";
+
+function Search() {
+  return (
+    <>
+      <div className="content">
+        <Row>
+          <Col md="12">
+            <Card>
+              <CardHeader>
+                <CardTitle tag="h4">Popular Movies</CardTitle>
+              </CardHeader>
+              <CardBody>
+              </CardBody>
+            </Card>
+          </Col>
+          <Col md="12">
+            <Card className="card-plain">
+              <CardHeader>
+                <CardTitle tag="h4">Action Movies</CardTitle>
+                <p className="category">Explosive Flicks</p>
+              </CardHeader>
+              <CardBody>
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+      </div>
+    </>
+  );
+}
+
+export default Search;
