3fb51472fd8de328ab3419a289a0a0e05d960a17
tyrellshawn <ooluk098@uottawa.ca>
2021-01-14 16:29:31 -0500

    Index and movie update

--- ShoppiesApp/src/models/MovieModel.js
+++ ShoppiesApp/src/models/MovieModel.js
@@ -1,4 +1,5 @@
-class MovieModel{
+import React, {Component} from "react";
+class MovieModel extends Component{
   title;
   imageUrl;
   releaseYear;
@@ -12,5 +13,16 @@ class MovieModel{
   }
   setNominated(update){
     this.nominated = update;
+  }
+  render(){
+    return`
+    <div class="col-md-3">
+      <div class="well text-center">
+        <img src="${movie.Poster}">
+        <h5>${movie.Title}</h5>
+        <a onclick="movieSelected('${movie.imdbID}')" class="btn btn-primary" href="#">Movie Details</a>
+      </div>
+    </div>
+  `;
   }
 }
